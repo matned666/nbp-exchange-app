da5d1ca40223e89fa889aff040f9d6cb14953c70
matni <mat.niedbal@yahoo.com>
2022-11-07 17:16:57 +0100

    parametrized tests

--- src/main/java/pl/mateusz/niedbal/nbpexchangeapp/controller/AppController.java
+++ src/main/java/pl/mateusz/niedbal/nbpexchangeapp/controller/AppController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.mateusz.niedbal.nbpexchangeapp.dto.CurrencyDTO;
 import pl.mateusz.niedbal.nbpexchangeapp.service.CurrencyService;
 
 import java.math.BigDecimal;
@@ -24,7 +23,8 @@ public class AppController {
                                 @RequestParam(defaultValue = "PLN") String currencyA,
                                 @RequestParam(defaultValue = "EUR") String currencyB) {
         BigDecimal exchange = currencyService.exchange(amount, currencyA, currencyB, date);
-        return "Exchange on " + date + ": " + amount + " " + currencyA.toUpperCase() + " = " + exchange + " " + currencyB.toUpperCase();
+        return "Exchange on " + date + ": " + amount + " "
+                + currencyA.toUpperCase() + " = " + exchange + " " + currencyB.toUpperCase();
     }
 
 }
--- src/test/java/pl/mateusz/niedbal/nbpexchangeapp/service/CurrencyServiceTest.java
+++ src/test/java/pl/mateusz/niedbal/nbpexchangeapp/service/CurrencyServiceTest.java
@@ -1,8 +1,10 @@
 package pl.mateusz.niedbal.nbpexchangeapp.service;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -12,6 +14,7 @@ import pl.mateusz.niedbal.nbpexchangeapp.api.model.RateModel;
 import pl.mateusz.niedbal.nbpexchangeapp.dto.CurrencyDTO;
 import pl.mateusz.niedbal.nbpexchangeapp.entity.Currency;
 import pl.mateusz.niedbal.nbpexchangeapp.repository.CurrencyRepository;
+import pl.mateusz.niedbal.nbpexchangeapp.utils.DateUtils;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -19,10 +22,8 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 
 @ExtendWith({SpringExtension.class})
 @SpringBootTest
@@ -37,21 +38,40 @@ class CurrencyServiceTest {
     @MockBean
     private ApiRequestService apiRequestService;
 
+    @Value("${api.currency.rate.url}")
+    private String apiUrl;
 
-    @Test
-    void ShouldReturnCurrencyFomApi_WhenNoCurrencyOnRepository(){
-        CurrencyModel currencyModel = new CurrencyModel("dolar", "usd", Collections.singletonList(new RateModel("2022-02-02", "5")));
-        Currency currency = new Currency("usd", "dolar", LocalDate.of(2022, 2, 2), new BigDecimal(5));
 
-        doReturn(currencyModel).when(apiRequestService).get(any());
-        doReturn(Optional.empty()).when(currencyRepository).findByCode("usd", LocalDate.of(2022, 2, 2));
-        doReturn(currency).when(currencyRepository).save(any());
+    @ParameterizedTest
+    @CsvSource({
+            "100, 20, USD, 1, EUR, 5, 2022-02-02",
+            "100, 20, PLN, 1, EUR, 5, 2022-02-02",
+            "100, 10, PLN, 1, EUR, 10, 2022-02-02",
+            "100, 66.67, USD, 10, EUR, 15, 2022-02-02",
+            "100, 6.67, PLN, 1000000, EUR, 15, 2022-02-02",
+            "100, 6.67, PLN, 34, EUR, 15, 2022-02-02",
+            "100, 20, USD, 1, EUR, 5, 2022-02-02"
+    })
+    void ShouldReturnCurrencyFomApi_WhenNoCurrencyOnRepository(String value, String expectedResultStr, String rateCodeA, String rateA, String rateCodeB, String rateB, String rateDate){
+        CurrencyModel currencyModelA = new CurrencyModel("test", rateCodeA, Collections.singletonList(new RateModel(rateDate, rateA)));
+        CurrencyModel currencyModelB = new CurrencyModel("test", rateCodeB, Collections.singletonList(new RateModel(rateDate, rateB)));
+        Currency currencyA = CurrencyDTO.applyApiModel(currencyModelA).convertToEntity();
+        Currency currencyB = CurrencyDTO.applyApiModel(currencyModelB).convertToEntity();
 
-        BigDecimal actualResult = currencyService.exchange(new BigDecimal(100), "pln", "usd", "2022-02-02");
-        BigDecimal expectedResult = new BigDecimal(20).divide(new BigDecimal(1), 2, RoundingMode.HALF_UP);
+        doReturn(currencyModelA).when(apiRequestService).get(apiUrl + rateCodeA +"/"+ rateDate +"/");
+        doReturn(currencyModelB).when(apiRequestService).get(apiUrl + rateCodeB +"/"+ rateDate +"/");
+        doReturn(Optional.empty()).when(currencyRepository).findByCode(rateCodeA, LocalDate.from(DateUtils.formatter.parse(rateDate)));
+        doReturn(Optional.empty()).when(currencyRepository).findByCode(rateCodeB, LocalDate.from(DateUtils.formatter.parse(rateDate)));
+        doReturn(currencyA).when(currencyRepository).save(currencyA);
+        doReturn(currencyB).when(currencyRepository).save(currencyB);
+
+        BigDecimal actualResult = currencyService.exchange(new BigDecimal(value), rateCodeA, rateCodeB, rateDate);
+        BigDecimal expectedResult = new BigDecimal(expectedResultStr).divide(new BigDecimal(1), 2, RoundingMode.HALF_UP);
 
         assertEquals(expectedResult, actualResult);
     }
+
+
 
 
 
